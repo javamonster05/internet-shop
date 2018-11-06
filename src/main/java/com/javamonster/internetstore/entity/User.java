1ab8e2cbfce54ef75f763b19de7648c4ffb0ef95
javamonster05 <kijkovdima@gmail.com>
2018-11-06 15:38:14 +0200

    sec commit

--- src/main/java/com/javamonster/internetstore/entity/User.java
+++ src/main/java/com/javamonster/internetstore/entity/User.java
@@ -1,4 +1,56 @@
 package com.javamonster.internetstore.entity;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name="user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "user_name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "user_password", nullable = false)
+    private String password;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return name;
+    }
+
+    public void setUsername(String username) {
+        this.name = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
